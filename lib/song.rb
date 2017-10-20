a2d967721e3a69d92dcb2eed0e4ae85bc2a313a6
Julianne Kim <juliannne@gmail.com>
2017-10-20 18:38:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
-  attr_accessor :name, :artist, :genre 
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
   
   def initialize(name, artist, genre)
-    @name = name 
+    @name = name
     @artist = artist
-    @genre = genre 
+    @genre = genre
   end
 end
